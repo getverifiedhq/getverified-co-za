26dc6f187e12ea32b69d62fcbf2a5036ac5fb691
Barend Erasmus <hirebarend@gmail.com>
2025-03-06 14:06:28 +0100

    update

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,11 @@
-export const NAME: string = "Get Verified";
+export const NAME: string =
+  { "getverified.co.za": "Get Verified", "proprvetting.com": "Propr Vetting" }[
+    window.location.hostname
+  ] || "Get Verified";
 
 export const HEADING: string = "Talk to an expert";
 
-export const SUBHEADING: string =
-  "Get Verified is South Africa’s trusted digital identity verification, fraud detection, and KYC compliance solution – helping businesses onboard customers securely and stay compliant.";
+export const SUBHEADING: string = `${NAME} is South Africa’s trusted digital identity verification, fraud detection, and KYC compliance solution – helping businesses onboard customers securely and stay compliant.`;
 
 export const SOCIAL_PROOF: string =
   "Trusted by leading South African businesses to verify thousands of identities every month.";
